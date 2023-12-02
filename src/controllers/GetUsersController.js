a564e8bd5e651809b49e29da6fc630be306ff53d
vitorrmarcelino <vitorrmarcelino09@gmail.com>
2023-12-02 13:51:19 -0300

    Ultimos ajustes

--- src/controllers/GetUsersController.js
+++ src/controllers/GetUsersController.js
@@ -2,10 +2,10 @@ const UserModel = require('../models/UserModel');
 
 exports.show = async (req, res) => {
   try {
-    const user = await UserModel.findOne({ _id: req.userID }).select('-senha');
+    const user = await UserModel.findOne({ _id: req.userId }).select('-senha');
     if (!user) return res.status(404).json({ mensagem: 'Nenhum usuário foi encontrado' });
     return res.json({ user });
   } catch (error) {
-    return res.status(500).json({ mensagem: 'Erro ao buscar usuário.' });
+    return res.status(500).json({ mensagem: 'Erro ao buscar usuários.' });
   }
 };
